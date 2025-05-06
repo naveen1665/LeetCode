5b07ac60e2a82c231f6c48feebfc5b27a5fd9714
Naveen <152183513+naveen1665@users.noreply.github.com>
2025-05-06 15:34:26 +0530

    Time: 0 ms (100%), Space: 39.6 MB (100%) - LeetHub

---
+++ 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countVowelStrings(int n) {
+        int[] dp = new int[]{1, 1, 1, 1, 1};
+        int ans = 0;
+
+        while (--n > 0){
+            for (int i = 3; i >= 0; i--){
+                dp[i] += dp[i + 1];
+            }
+        }
+        for(int x : dp) {
+            ans += x;
+        }
+        return ans;
+    }
+}
