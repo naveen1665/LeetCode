1d854e6d8a141aaad8b2652bf2c8da752e62c4fb
Naveen <152183513+naveen1665@users.noreply.github.com>
2025-07-20 21:34:28 +0530

    Time: 3 ms (100%), Space: 85.7 MB (5.89%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,25 +1,41 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        Queue<Integer> pos=new LinkedList<>();
-        Queue<Integer> neg=new LinkedList<>();
-        for(int num : nums)
+        // Queue<Integer> pos=new LinkedList<>();
+        // Queue<Integer> neg=new LinkedList<>();
+        // for(int num : nums)
+        // {
+        //     if(num<0)
+        //     {
+        //         neg.add(num);
+        //     }
+        //     else
+        //     {
+        //         pos.add(num);
+        //     }
+        // }
+        // int i=0;
+        // while(i<nums.length && !pos.isEmpty() && !neg.isEmpty())
+        // {
+        //     nums[i++]=pos.remove();
+        //     nums[i++]=neg.remove();
+        // }
+        // return nums;
+        int n=nums.length;
+        int [] res=new int[n];
+        int i=0,j=1;
+        for(int k=0;k<n;k++)
         {
-            if(num<0)
+            if(nums[k]>0)
             {
-                neg.add(num);
+                res[i]=nums[k];
+                i=i+2;
             }
             else
             {
-                pos.add(num);
+                res[j]=nums[k];
+                j=j+2;
             }
         }
-        int i=0;
-        while(i<nums.length && !pos.isEmpty() && !neg.isEmpty())
-        {
-            nums[i++]=pos.remove();
-            nums[i++]=neg.remove();
-        }
-        return nums;
-        
+        return res;
     }
 }
