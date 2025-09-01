f08876d30056f303ebc9250d020984a63c2f1537
Naveen <152183513+naveen1665@users.noreply.github.com>
2025-09-01 23:23:32 +0530

    Time: 50 ms (5.06%), Space: 46.4 MB (5.98%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,40 +1,73 @@
+// class Solution {
+//     public String convert(String s, int numRows) {
+//         char [][]arr=new char[numRows][s.length()];
+//         int n=s.length();
+//         int row=0,col=0,curr=0;
+//         while(curr<n)
+//         {
+//             while(curr<n &&row<numRows)
+//             {
+//                 arr[row++][col]=s.charAt(curr++);
+//             }
+//             row-=2;
+//             row=Math.max(0,row);
+//             while(curr<n && row>0)
+//             {
+//                 arr[row--][++col]=s.charAt(curr++);
+//             }
+//             col++;
+//         }
+//         StringBuffer res=new StringBuffer();
+//         for(int i=0;i<numRows;i++)
+//         {
+//             for(int j=0;j<s.length();j++)
+//             {
+//                 if(arr[i][j]!=0)
+//                 {
+//                     System.out.print(" ");
+//                     res.append(arr[i][j]);
+//                     continue;
+//                 }
+//                 System.out.print(arr[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+        
+//         return res.toString();
+//     }
+// }
+
+
 class Solution {
     public String convert(String s, int numRows) {
-        char [][]arr=new char[numRows][s.length()];
-        int n=s.length();
-        int row=0,col=0,curr=0;
-        while(curr<n)
+        char [][] arr=new char[numRows][s.length()];
+        int curr=0,row=0,col=0;
+        while(curr<s.length())
         {
-            while(curr<n &&row<numRows)
+            while(curr<s.length() && row<numRows)
             {
                 arr[row++][col]=s.charAt(curr++);
             }
             row-=2;
             row=Math.max(0,row);
-            col++;
-            while(curr<n && row>=1)
+            while(curr<s.length() && row>0)
             {
-                arr[row--][col++]=s.charAt(curr++);
+                arr[row--][++col]=s.charAt(curr++);
             }
-            row=0;
             col++;
         }
-        StringBuffer res=new StringBuffer();
-        for(int i=0;i<arr.length;i++)
+        StringBuilder res=new StringBuilder();
+        for(int i=0;i<numRows;i++)
         {
-            for(int j=0;j<arr[0].length;j++)
+            for(int j=0;j<s.length();j++)
             {
                 if(arr[i][j]!=0)
                 {
-                    System.out.print(" ");
                     res.append(arr[i][j]);
-                    continue;
                 }
-                System.out.print(arr[i][j]+" ");
             }
-            System.out.println();
         }
-        
         return res.toString();
+        
     }
 }
