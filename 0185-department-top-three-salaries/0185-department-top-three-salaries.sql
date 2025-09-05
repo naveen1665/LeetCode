503b1dc3c9263d81b8cb26a1cfa70fc541e6bbeb
Naveen <152183513+naveen1665@users.noreply.github.com>
2025-09-05 17:37:54 +0530

    Time: 823 ms (96.14%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,4 +1,7 @@
-select e.name as Employee,
-d.name as Department,
-e.salary as salary
-from( select departmentId,name,salary, dense_rank() over(partition by departmentId order by salary desc ) as rnk from Employee) e join Department d on e.departmentId=d.id where rnk<=3;
+SELECT d.name AS Department,
+e.name AS Employee,
+e.salary AS Salary
+from( 
+    SELECT departmentId,name,salary, dense_rank() over (
+        PARTITION BY departmentId ORDER BY salary desc) as rnk from Employee 
+) e join Department d on d.id=e.departmentId where rnk<=3;
