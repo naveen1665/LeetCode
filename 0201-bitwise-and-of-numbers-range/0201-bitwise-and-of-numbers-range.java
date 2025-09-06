af4a6f3574780f5d882b981f5d72e59fb860702c
Naveen <152183513+naveen1665@users.noreply.github.com>
2025-09-06 23:32:34 +0530

    Time: 3 ms (100%), Space: 44 MB (59.6%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -1,34 +1,47 @@
-class Solution {
-    public static int pos(int n)
-    {
-        int pos=-1;
-        while(n!=0)
-        {
-            n=n>>1;
-            pos++;
-        }
-        return pos;
-    }
+// class Solution {
+//     public static int pos(int n)
+//     {
+//         int pos=-1;
+//         while(n!=0)
+//         {
+//             n=n>>1;
+//             pos++;
+//         }
+//         return pos;
+//     }
 
-    public int rangeBitwiseAnd(int left, int right) {
+//     public int rangeBitwiseAnd(int left, int right) {
 
-        int res=0;
+//         int res=0;
 
-        while(left>0 && right>0)
-        {
+//         while(left>0 && right>0)
+//         {
         
-            int l_pos=pos(left);
-            int r_pos=pos(right);
-            if(l_pos!=r_pos)
-            {
-                break;
-            }
-            int msb_val=1<<l_pos;
-            res+=msb_val;
+//             int l_pos=pos(left);
+//             int r_pos=pos(right);
+//             if(l_pos!=r_pos)
+//             {
+//                 break;
+//             }
+//             int msb_val=1<<l_pos;
+//             res+=msb_val;
 
-            left-=msb_val;
-            right-=msb_val;
+//             left-=msb_val;
+//             right-=msb_val;
+//         }
+//         return res;
+//     }
+// }
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        while (left < right) {
+            left >>= 1;   
+            right >>= 1;
+            shift++;      
         }
-        return res;
+
+        return left << shift;
     }
 }
